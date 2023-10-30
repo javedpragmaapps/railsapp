2db344fa860ef1a1e6ee4bc592a4908871092008
Javed Usmani <javedusmani@peoplebrowsr.com>
2023-10-30 17:38:00 +0530

    Inside a post add a form to contact a user - Define a contact_user_partial_path helper method in PostsHelper.   Add specs for the method - Create _contact_user.html.erb and _login_required.html.erb partials - Define a leave_message_partial_path helper method in PostsHelper.   Add specs for the method - Create _already_in_touch.html.erb and _message_form.html.erb   partial files - Define a @message_has_been_sent in PostsController's show action - Define a between_users scope inside the Private::Conversation model   Add specs for the scope - Define private_conversation and private_message factories - Define routes for Private::Conversations and Private::Messages - Define a create action inside the Private::Conversations - Create _success.js and _fail.js partials - Add feature specs to test the overall .contact-user form

--- spec/factories/private_messages.rb
+++ spec/factories/private_messages.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :private_message, class: 'Private::Message' do
     
+    body 'a' * 20
+    association :conversation, factory: :private_conversation
+    user
+    
   end
 end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -62,17 +62,65 @@ RSpec.describe PostsHelper, type: :helper do
   end
 
   context '#post_format_partial_path' do
-  it "returns a home_page partial's path" do
-    helper.stub(:current_page?).and_return(true)
-    expect(helper.post_format_partial_path).to (
-      eq 'posts/post/home_page'
+    it "returns a home_page partial's path" do
+      helper.stub(:current_page?).and_return(true)
+      expect(helper.post_format_partial_path).to (
+        eq 'posts/post/home_page'
+      )
+    end
+
+    it "returns a branch_page partial's path" do
+      helper.stub(:current_page?).and_return(false)
+      expect(helper.post_format_partial_path).to (
+        eq 'posts/post/branch_page'
+      )
+    end
+  end
+
+  context '#contact_user_partial_path' do
+    before(:each) do
+      @current_user = create(:user, id: 1)
+      helper.stub(:current_user).and_return(@current_user)
+    end
+
+    it "returns a contact_user partial's path" do
+      helper.stub(:user_signed_in?).and_return(true)
+      assign(:post, create(:post, user_id: create(:user, id: 2).id))
+      expect(helper.contact_user_partial_path).to(
+        eq 'posts/show/contact_user' 
+      )
+    end
+
+    it "returns an empty partial's path" do
+      helper.stub(:user_signed_in?).and_return(true)
+      assign(:post, create(:post, user_id: @current_user.id))
+
+      expect(helper.contact_user_partial_path).to(
+        eq 'shared/empty_partial'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      helper.stub(:user_signed_in?).and_return(false)
+      expect(helper.contact_user_partial_path).to(
+        eq 'posts/show/login_required'
+      )
+    end
+  end
+
+
+context '#leave_message_partial_path' do
+  it "returns an already_in_touch partial's path" do
+    assign('message_has_been_sent', true)
+    expect(helper.leave_message_partial_path).to(
+      eq 'posts/show/contact_user/already_in_touch'
     )
   end
 
-  it "returns a branch_page partial's path" do
-    helper.stub(:current_page?).and_return(false)
-    expect(helper.post_format_partial_path).to (
-      eq 'posts/post/branch_page'
+  it "returns an already_in_touch partial's path" do
+    assign('message_has_been_sent', false)
+    expect(helper.leave_message_partial_path).to(
+      eq 'posts/show/contact_user/message_form'
     )
   end
 end
